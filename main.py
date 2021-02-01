600e9e523dded584f9972f4685611fea4ca81a54
Giorgouis <69904185+Giorgouis@users.noreply.github.com>
2021-02-01 21:01:28 +0200

    multiple channel exceptions can be set as input
    You can now use more than 1 channel IDs as exceptions using ', ' to seperate them

--- main.py
+++ main.py
@@ -10,23 +10,30 @@ async def on_ready():
 
 @client.command()
 @commands.has_permissions(manage_channels=True, administrator=True)
-async def delete_channels(ctx, channel_name, *, exception=None):
+async def delete_channels(ctx, channel_name, *, exceptions='None'):
     deleted_channels = 0
-    has_exceptions = False
-    if exception is None:
+    if exceptions == 'None':
+        guild = client.get_guild(ctx.guild.id)
         guild = client.get_guild(ctx.guild.id)
         for channel in ctx.guild.text_channels:
+            is_exception = False
             if channel.name == channel_name:
                 await channel.delete()
                 deleted_channels += 1
+        if deleted_channels == 0:
+            await ctx.send("Couldn't find any channels with that name")
+        else:
+            await ctx.send(f'Found and deleted {deleted_channels} channels')
     else:
+        exceptions_cont = exceptions.split(', ')
         guild = client.get_guild(ctx.guild.id)
         for channel in ctx.guild.text_channels:
             is_exception = False
             if channel.name == channel_name:
 
-                if int(channel.id) == int(exception):
-                    is_exception = True
+                for exception in exceptions_cont:
+                    if int(channel.id) == int(exception):
+                        is_exception = True
 
                 if is_exception:
                     pass
