edb03c2563c803dfd38643f725671c2ee5d5d96f
Giorgouis <69904185+Giorgouis@users.noreply.github.com>
2021-02-03 20:22:57 +0200

    added delete_roles command

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ async def delete_channels(ctx, channel_name, *, exceptions='None'):
     deleted_channels = 0
     if exceptions == 'None':
         guild = client.get_guild(ctx.guild.id)
-        guild = client.get_guild(ctx.guild.id)
         for channel in ctx.guild.text_channels:
             is_exception = False
             if channel.name == channel_name:
@@ -41,10 +40,48 @@ async def delete_channels(ctx, channel_name, *, exceptions='None'):
                     await channel.delete()
                     deleted_channels += 1
 
-    if deleted_channels == 0:
-        await ctx.send("Couldn't find any channels with that name")
+        if deleted_channels == 0:
+            await ctx.send("Couldn't find any channels with that name")
+        else:
+            await ctx.send(f'Found and deleted {deleted_channels} channels')
+
+
+@client.command()
+@commands.has_permissions(manage_roles=True, administrator=True)
+async def delete_roles(ctx, role_name, *, exceptions='None'):
+    deleted_roles = 0
+    if exceptions == 'None':
+        guild = client.get_guild(ctx.guild.id)
+        for role in ctx.guild.roles:
+            is_exception = False
+            if role.name == role_name:
+                await role.delete()
+                deleted_roles += 1
+        if deleted_roles == 0:
+            await ctx.send("Couldn't find any channels with that name")
+        else:
+            await ctx.send(f'Found and deleted {deleted_roles} channels')
     else:
-        await ctx.send(f'Found and deleted {deleted_channels} channels')
+        exceptions_cont = exceptions.split(', ')
+        guild = client.get_guild(ctx.guild.id)
+        for role in ctx.guild.roles:
+            is_exception = False
+            if role.name == role_name:
+
+                for exception in exceptions_cont:
+                    if int(role.id) == int(exception):
+                        is_exception = True
+
+                if is_exception:
+                    pass
+                else:
+                    await role.delete()
+                    deleted_roles += 1
+
+        if deleted_roles == 0:
+            await ctx.send("Couldn't find any channels with that name")
+        else:
+            await ctx.send(f'Found and deleted {deleted_roles} channels')
 
 
 client.run(TOKEN)
